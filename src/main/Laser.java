542c0f9f2f9afcbc0f0e4af09f1960d88f48821d
jdm13003 <jonathon.mcrae@uconn.edu>
2016-04-15 14:38:05 -0400

    I fix

--- src/main/Laser.java
+++ src/main/Laser.java
@@ -17,12 +17,12 @@ public class Laser {
 		
 		SpriteSheet ss = new SpriteSheet(game.getSpriteSheet());
 		
-		laser = ss.grabimage(1, 1, 64, 64);
+		laser = ss.grabimage(2, 3, 64, 64);
 	}
 	
 	public void tick(){
-		x+=1;
-		y+=1;
+		x+=10;
+		//y+=1;
 	}
 	public void render(Graphics g){
 		g.drawImage(laser, (int)x, (int)y, null);
